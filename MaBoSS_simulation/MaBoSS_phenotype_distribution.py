aaae363e366b2b86a52eee811f0d5b0347af1caf
Romane Gauthier <romane1858@gmail.com>
2025-05-26 12:33:39 +0100

    added cnv to personalize boolean validation

--- MaBoSS_simulation/MaBoSS_phenotype_distribution.py
+++ MaBoSS_simulation/MaBoSS_phenotype_distribution.py
@@ -5,15 +5,21 @@ import pandas as pd
 import numpy as np
 
 
-def compute_phenotypes_distribution(folder,dir_files, inputs_list, group_label, drug_interest):
-    output_dir = f'{folder}/{group_label}_results/only_gene_expression/single_input_on/phenotype_distribution_patients'
+def compute_phenotypes_distribution(
+    folder, dir_files, inputs_list, drug_interest, group_label=None
+):
+    if group_label is not None:
+        output_dir = f"{folder}/{group_label}_results/only_gene_expression/single_input_on/phenotype_distribution_patients"
+    else:
+        output_dir = f"{folder}/only_gene_expression/single_input_on/phenotype_distribution_patients"
+
     cfg_files = [f for f in os.listdir(dir_files) if f.endswith(f"{drug_interest}.cfg")]
     patients_data_dict = {}
     for cfg_file in cfg_files:
         cfg_path = os.path.join(dir_files, cfg_file)
         base_name = os.path.splitext(cfg_file)[0]
         bnd_path = os.path.join(dir_files, base_name + ".bnd")
-        #model = maboss.load(bnd_path, cfg_path)
+        # model = maboss.load(bnd_path, cfg_path)
         patients_results = pd.DataFrame()
         print(f"\n--- Results for patient: {base_name} ---")
 
@@ -29,12 +35,14 @@ def compute_phenotypes_distribution(folder,dir_files, inputs_list, group_label, 
             last_state = result.get_last_states_probtraj()
             last_state.index = [f"{active_input}_ON"]
             patients_results = pd.concat([patients_results, last_state])
-            expected_cols = ['<nil>', 'Apoptosis', 'Proliferation', 'Metastasis']
-            available_cols = [col for col in expected_cols if col in patients_results.columns]
+            expected_cols = ["<nil>", "Apoptosis", "Proliferation", "Metastasis"]
+            available_cols = [
+                col for col in expected_cols if col in patients_results.columns
+            ]
             patients_results = patients_results[available_cols]
             patients_results = patients_results.fillna(0)
 
-                # Save to CSV
+            # Save to CSV
             output_path = os.path.join(output_dir, f"{base_name}_phenotypes.csv")
             patients_results.to_csv(output_path)
 
@@ -44,27 +52,56 @@ def compute_phenotypes_distribution(folder,dir_files, inputs_list, group_label, 
 
         patients_data_dict[base_name] = patients_results
         print(patients_results)
-        
 
     return patients_data_dict
-
-
 
 
 def compute_mean_patients(dic_patients_data):
     stats_results_data = {}
     results = {}
     flags_end = [False, False, False]
-    df_results_mean = pd.DataFrame(index=['EGF_ON', 'FGF_ON', 'TGFb_ON', 'Nutrients_ON', 'Hypoxia_ON', 'Acidosis_ON', 'Androgen_ON', 'TNFalpha_ON', 'Carcinogen_ON'], columns=['<nil>', 'Apoptosis', 'Proliferation', 'Metastasis'])
-    df_results_std = pd.DataFrame(index=['EGF_ON', 'FGF_ON', 'TGFb_ON', 'Nutrients_ON', 'Hypoxia_ON', 'Acidosis_ON', 'Androgen_ON', 'TNFalpha_ON', 'Carcinogen_ON'], columns=['<nil>', 'Apoptosis', 'Proliferation', 'Metastasis'])
+    df_results_mean = pd.DataFrame(
+        index=[
+            "EGF_ON",
+            "FGF_ON",
+            "TGFb_ON",
+            "Nutrients_ON",
+            "Hypoxia_ON",
+            "Acidosis_ON",
+            "Androgen_ON",
+            "TNFalpha_ON",
+            "Carcinogen_ON",
+        ],
+        columns=["<nil>", "Apoptosis", "Proliferation", "Metastasis"],
+    )
+    df_results_std = pd.DataFrame(
+        index=[
+            "EGF_ON",
+            "FGF_ON",
+            "TGFb_ON",
+            "Nutrients_ON",
+            "Hypoxia_ON",
+            "Acidosis_ON",
+            "Androgen_ON",
+            "TNFalpha_ON",
+            "Carcinogen_ON",
+        ],
+        columns=["<nil>", "Apoptosis", "Proliferation", "Metastasis"],
+    )
 
-    for idx_patient, (patient_id, patient_data) in enumerate(dic_patients_data.items(), 1):
+    for idx_patient, (patient_id, patient_data) in enumerate(
+        dic_patients_data.items(), 1
+    ):
         if len(dic_patients_data) == idx_patient:
             flags_end[0] = True
-        for idx_condition, (condition_name, phenotypes) in enumerate(patient_data.iterrows(), 1):
+        for idx_condition, (condition_name, phenotypes) in enumerate(
+            patient_data.iterrows(), 1
+        ):
             if patient_data.shape[0] == idx_condition:
                 flags_end[1] = True
-            for idx_phenotype, (phenotype_name, phenotype_value) in enumerate(phenotypes.items(), 1):
+            for idx_phenotype, (phenotype_name, phenotype_value) in enumerate(
+                phenotypes.items(), 1
+            ):
                 if len(phenotypes.values) == idx_phenotype:
                     flags_end[2] = True
                 if condition_name not in results.keys():
@@ -75,27 +112,27 @@ def compute_mean_patients(dic_patients_data):
                 results[condition_name][phenotype_name].append(phenotype_value)
                 temp_result = results[condition_name][phenotype_name]
 
-
                 # Stats test table
                 if condition_name not in stats_results_data.keys():
                     stats_results_data[condition_name] = {}
                 if phenotype_name not in stats_results_data[condition_name].keys():
                     stats_results_data[condition_name][phenotype_name] = []
-                
-                stats_results_data[condition_name][phenotype_name].append(phenotype_value)
+
+                stats_results_data[condition_name][phenotype_name].append(
+                    phenotype_value
+                )
 
                 if False not in flags_end:
                     mean = np.mean(temp_result)
                     std = np.std(temp_result, ddof=1)
 
-                    #results[condition_name][phenotype_name] = [mean, std]
-                    # Put mean and std in dataframe
+                    # results[condition_name][phenotype_name] = [mean, std]
+                    # Put mean and std in dataframe
 
                     if pd.isna(df_results_mean.at[condition_name, phenotype_name]):
                         df_results_mean.at[condition_name, phenotype_name] = mean
                     else:
                         df_results_mean.at[condition_name, phenotype_name] += mean
-                
 
                     # Same for std
                     if pd.isna(df_results_std.at[condition_name, phenotype_name]):
@@ -104,11 +141,7 @@ def compute_mean_patients(dic_patients_data):
                         df_results_std.at[condition_name, phenotype_name] += std
 
     stats_results_data_df = pd.DataFrame(stats_results_data)
-    print('Stats test table values')
+    print("Stats test table values")
     print(stats_results_data_df)
 
     return df_results_mean.round(2), df_results_std.round(2), stats_results_data_df
-
-
-
-
--- pre_process_data/tcga_preprocess_data.py
+++ pre_process_data/tcga_preprocess_data.py
@@ -1,44 +1,104 @@
-import pandas as pd 
 import numpy as np
+import mygene
 
 
-#phenotype_data = pd.read_csv('../data/TCGA_data/tcga_gdc_pancan_phenotype.csv')
-tumor_stage_data = pd.read_csv('../data/TCGA_data/TCGA_GDC-PANCAN_tumor_stage_phenotype.csv')
-genes_data = pd.read_csv('../data/TCGA_data/TCGA_GDC-PANCAN_genes.csv')
+def pre_process_tcga_data(cnv_data, genes_data, patients_id, montagud_nodes):
+    cnv_col = ["Unnamed: 0"] + patients_id
+    genes_col = ["xena_sample"] + patients_id
 
+    cnv_data_filtered = cnv_data[cnv_col]
+    genes_data_filtered = genes_data[genes_col]
 
+    # filter cnv data
+    ensembl_ids_cnv = list(cnv_data_filtered["Unnamed: 0"])
+    ensembl_ids_cnv = [eid.split(".")[0] for eid in ensembl_ids_cnv]
+    mg = mygene.MyGeneInfo()
+    result = mg.querymany(
+        ensembl_ids_cnv, scopes="ensembl.gene", fields="symbol", species="human"
+    )
+    cnv_symbols = {item["query"]: item.get("symbol", None) for item in result}
+    cnv_names = list(cnv_symbols.values())
+    cnv_data_filtered["gene_name"] = cnv_names
 
+    cnv_data_filtered = cnv_data_filtered[cnv_data_filtered["gene_name"].notna()]
+    cnv_data_filtered = cnv_data_filtered.drop(columns=["Unnamed: 0"])
 
-# print(tumor_stage_data.head())
-# print(tumor_stage_data['diagnoses.tumor_stage'].value_counts())
+    cnv_data_filtered = cnv_data_filtered.set_index("gene_name")
+    common_elements = list(set(cnv_data_filtered.index) & set(montagud_nodes))
+    cnv_data_filtered = cnv_data_filtered.loc[common_elements]
 
-patients_ids = list(set(tumor_stage_data['sample']))
+    cnv_data_filtered = cnv_data_filtered.transpose()
 
-patients_group_df = pd.DataFrame(index=patients_ids)
+    cnv_data_filtered.index.name = "samples_id"
+    cnv_data_filtered = cnv_data_filtered.reset_index()
 
-group_0 = ['stage 0']
-group_1 = ['stage i', 'stage ia', 'stage ib']
-group_2 = ['stage ii', 'stage iia', 'stage iib', 'stage iic']
-group_3 = ['stage iii', 'stage iiia', 'stage iiib', 'stage iiic']
-group_4 = ['stage iv', 'stage iva', 'stage ivb', 'stage ivc']
+    df_melted_cnv = cnv_data_filtered.melt(
+        id_vars=["samples_id"], var_name="gene_name", value_name="expression_value"
+    )
 
-conditions = [
-    tumor_stage_data['diagnoses.tumor_stage'].isin(group_0),
-    tumor_stage_data['diagnoses.tumor_stage'].isin(group_1),
-    tumor_stage_data['diagnoses.tumor_stage'].isin(group_2),
-    tumor_stage_data['diagnoses.tumor_stage'].isin(group_3),
-    tumor_stage_data['diagnoses.tumor_stage'].isin(group_4) 
+    df_melted_cnv = df_melted_cnv.rename(
+        columns={
+            "samples_id": "model_id",
+            "gene_name": "gene_symbol",
+            "expression_value": "rsem_tpm",
+        }
+    )
+    group_loss = [-1]
+    group_normal = [0]
+    group_gain = [1]
+
+    conditions = [
+        df_melted_cnv["rsem_tpm"].isin(group_loss),
+        df_melted_cnv["rsem_tpm"].isin(group_normal),
+        df_melted_cnv["rsem_tpm"].isin(group_gain),
     ]
-choices = ['Local Cancer', 'Early Stage', 'Larger Tumor', 'Advanced Local Speed', 'Metastatic']
+    choices = ["Loss", "Normal", "Gain"]
+    df_melted_cnv.loc[:, "effect"] = np.select(conditions, choices, default="")
+    df_melted_cnv.to_csv("../data/TCGA_data/filtered_data/cnv_samples_table.csv")
 
-patients_group_df.loc[:,'Tumor Stage'] = np.select(conditions, choices, default = '')
-patients_group_df = patients_group_df[patients_group_df['Tumor Stage'].notna() & (patients_group_df['Tumor Stage'] != '')]
-# print(patients_group_df['Tumor Stage'].value_counts())
-patients_ids = patients_group_df.index.tolist()
-print(patients_ids)
+    # # Filter genes dataset
+    ensembl_ids_gene = list(genes_data_filtered["xena_sample"])
+    ensembl_ids_gene = [eid.split(".")[0] for eid in ensembl_ids_gene]
+    mg = mygene.MyGeneInfo()
+    result = mg.querymany(
+        ensembl_ids_gene, scopes="ensembl.gene", fields="symbol", species="human"
+    )
+    gene_symbols = {item["query"]: item.get("symbol", None) for item in result}
+    genes_names = list(gene_symbols.values())
+    genes_data_filtered["gene_name"] = genes_names
+    genes_data_filtered = genes_data_filtered[genes_data_filtered["gene_name"].notna()]
+    genes_data_filtered = genes_data_filtered.drop(columns=["xena_sample"])
 
-print(patients_group_df.head())
-print(genes_data)
-# filter genes to only keep the patients_ids info
-genes_data_filtered = genes_data[patients_ids]
-print(genes_data_filtered)
+    genes_data_filtered = genes_data_filtered.set_index("gene_name")
+    genes_data_filtered = genes_data_filtered.transpose()
+
+    genes_data_filtered = genes_data_filtered.dropna(axis=1)
+    genes_data_filtered = genes_data_filtered.dropna(axis=0)
+
+    threshold = 0.5
+    zero_proportion = (genes_data_filtered == 0).sum() / len(genes_data_filtered)
+    genes_data_filtered_filtered = genes_data_filtered.loc[
+        :, zero_proportion <= threshold
+    ]
+
+    # keep only the genes in montagud nodes
+    genes_data_ids = list(genes_data_filtered_filtered.columns)
+
+    common_elements = list(set(genes_data_ids) & set(montagud_nodes))
+    genes_data_filtered = genes_data_filtered_filtered[common_elements]
+
+    genes_data_filtered.index.name = "samples_id"
+    genes_data_filtered = genes_data_filtered.reset_index()
+    df_melted_genes = genes_data_filtered.melt(
+        id_vars=["samples_id"], var_name="gene_name", value_name="expression_value"
+    )
+
+    df_melted_genes = df_melted_genes.rename(
+        columns={
+            "samples_id": "model_id",
+            "gene_name": "gene_symbol",
+            "expression_value": "rsem_tpm",
+        }
+    )
+    df_melted_genes.to_csv("../data/TCGA_data/filtered_data/genes_samples_table.csv")
+    return df_melted_cnv, df_melted_genes
